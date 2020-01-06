7adda957906223e5b9f95b65838ddd99a28f4428
Patryk Rzucidlo (PTKDev) <ptkdev@gmail.com>
2020-01-06 17:07:59 +0100

    [Update] popup (client)

---
+++ client/js/events.js
@@ -0,0 +1,1 @@
+bulmaTagsinput.attach();
--- gulpfile.js
+++ gulpfile.js
@@ -16,7 +16,7 @@ const gulp_minifyjs = require("gulp-terser");
 const gulp_rename = require("gulp-rename");
 
 gulp.task("build-css", function() {
-	return gulp.src([`./node_modules/bulma/css/bulma.min.css`, `./client/css/main.scss`])
+	return gulp.src([`./node_modules/bulma/css/bulma.min.css`, `./node_modules/bulma-extensions/dist/css/bulma-extensions.min.css`, `./node_modules/bulma-extensions/bulma-tagsinput/dist/css/bulma-tagsinput.min.css`, `./client/css/main.scss`, `./client/css/colors.scss`])
 		.pipe(gulp_concat({path: "./full.min.tmp"}))
 		.pipe(gulp_sass())
 		.pipe(gulp_minifycss())
@@ -25,12 +25,22 @@ gulp.task("build-css", function() {
 });
 
 gulp.task("build-js-popup", function() {
-	let files = [`./client/js/dom.js`, `./client/js/main.js`, `./client/js/events.js`, `./client/js/video.js`];
+	let files = [`./node_modules/bulma-extensions/dist/js/bulma-extensions.min.js`, `./node_modules/bulma-extensions/bulma-tagsinput/dist/js/bulma-tagsinput.min.js`, `./client/js/dom.js`, `./client/js/main.js`, `./client/js/events.js`];
 
 	return gulp.src(files)
 		.pipe(gulp_concat({path: "popup.min.tmp"}))
-		.pipe(gulp_minifyjs())
+		.pipe(gulp_minifyjs({output: {comments: false}}))
 		.pipe(gulp_rename("popup.min.js"))
+		.pipe(gulp.dest(`./build/js/`));
+});
+
+gulp.task("build-js-background", function() {
+	let files = [`./client/js/video.js`];
+
+	return gulp.src(files)
+		.pipe(gulp_concat({path: "video.min.tmp"}))
+		.pipe(gulp_minifyjs({output: {comments: false}}))
+		.pipe(gulp_rename("video.min.js"))
 		.pipe(gulp.dest(`./build/js/`));
 });
 
@@ -39,7 +49,7 @@ gulp.task("build-js-options", function() {
 
 	return gulp.src(files)
 		.pipe(gulp_concat({path: "options.min.tmp"}))
-		.pipe(gulp_minifyjs())
+		.pipe(gulp_minifyjs({output: {comments: false}}))
 		.pipe(gulp_rename("options.min.js"))
 		.pipe(gulp.dest(`./build/js/`));
 });
@@ -74,14 +84,15 @@ gulp.task("build-manifest", function() {
 
 gulp.task("build-watch", (done) => {
 	gulp.watch([`./client/css/**/*.scss`]).on("change", gulp.parallel("build-css"));
-	gulp.watch([`./client/js/**/*.js`]).on("change", gulp.parallel("build-js-popup", "build-js-options"));
+	gulp.watch([`./client/js/**/*.js`]).on("change", gulp.parallel("build-js-popup", "build-js-options", "build-js-background"));
 	gulp.watch([`./client/images/**/*`]).on("change", gulp.parallel("build-images"));
+	gulp.watch([`./client/html/**/*`]).on("change", gulp.parallel("build-html"));
 	gulp.watch([`./translations/**/*`]).on("change", gulp.parallel("build-translations"));
 	gulp.watch([`./configs/**/*`]).on("change", gulp.parallel("build-manifest"));
 
 	done();
 });
 
-gulp.task("build", gulp.series("build-css", "build-js-popup", "build-js-options", "build-html", "build-images", "build-translations", "build-manifest"));
+gulp.task("build", gulp.series("build-css", "build-js-popup", "build-js-options", "build-js-background", "build-html", "build-images", "build-translations", "build-manifest"));
 
 gulp.task("dev", gulp.parallel("build-watch"));
